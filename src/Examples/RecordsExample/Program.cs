c00126d79666e10f673b38dd649f16c0c0f2ab1c
Russ Koprulu <rustulp@gmail.com>
2023-07-19 01:48:24 -0600

    added records example program

---
+++ src/Examples/RecordsExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using DnsServerAPI;
+
+const string ADDRESS = "http://localhost:5380";
+const string USER = "admin";
+const string PASS = "admin";
+
+string token = await Auth.GetLoginToken(ADDRESS, USER, PASS);
+
+Api api = new(ADDRESS, token);
+
+
+Console.WriteLine("[CREATE TXT RECORD]");
+string res = await api.AddRecord("example.did", "TXT", 600, false, "a test comment", "&text=foo");
+
+Console.WriteLine(res);
+Console.ReadLine();
