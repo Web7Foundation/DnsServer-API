9c8ce0121aaf8eb6671c29e8e1211b4236bda939
Russ Koprulu <rustulp@gmail.com>
2023-07-23 00:44:33 -0600

    added method

--- src/DnsServerAPI/DnsClient.cs
+++ src/DnsServerAPI/DnsClient.cs
@@ -1,9 +1,22 @@
 ﻿
 namespace DnsServerAPI;
 
+public enum Protocol
+{
+    Udp, 
+    Tcp,
+    Tls,
+    Https,
+    Quic
+}
+
 public static class DnsClient
 {
     // TODO: Implement DNS Client 
 
     // https://github.com/TechnitiumSoftware/DnsServer/blob/master/APIDOCS.md#dns-client-api-calls
+    public static async Task<string> ResolveQuery(string token, string server, string domain, Protocol protocol = Protocol.Udp)
+    {
+        throw new NotImplementedException();
+    }
 }
