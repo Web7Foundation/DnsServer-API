1d41d3c31ccd2cbe0d153cb356c00a5bcf9290ee
Russ Koprulu <rustulp@gmail.com>
2023-07-23 00:44:52 -0600

    refactor

--- src/Examples/RecordsExample/Program.cs
+++ src/Examples/RecordsExample/Program.cs
@@ -8,37 +8,35 @@ string token = await Auth.GetLoginToken(ADDRESS, USER, PASS);
 
 Api api = new(ADDRESS, token);
 
-
-Console.WriteLine("[CREATE TXT RECORD]");
-
 // see https://github.com/Web7Foundation/DnsServer/blob/master/APIDOCS.md#add-record
 // for a list of url parameters of all the DNS record types
 
 // here is a TXT record example
-//await api.AddRecord("example.did", "TXT", 600, false, "a test comment", "&text=foo");
+await api.AddRecord("example.did", "1234.example.did", "TXT", 600, false, "a test comment", "&text=foo");
 
 // DIDID (the DID value will be the the zoneName value you input ex. 'did:example:1234')
-//await api.AddRecord("did:example:1234", "DIDID", 600, false, null, null);
+await api.AddRecord("did:example:1234", "did:example:1234", "DIDID", 600, false, null, null);
 
 // DIDPURP
-//await api.AddRecord("did:example", "DIDPURP", 600, false, null, "&purpose=the purpose of this is to be an example...");
+await api.AddRecord("did:example", "did:example:1234", "DIDPURP", 600, false, null, "&purpose=the purpose of this is to be an example...");
 
 // DIDCOMM
-//await api.AddRecord("did:example", "DIDCOMM", 600, false, null, "&comment=this is a DID comment");
+await api.AddRecord("did:example", "did:example:1234", "DIDCOMM", 600, false, null, "&comment=this is a DID comment");
 
 // DIDCTXT
-//await api.AddRecord("did:example", "DIDCTXT", 600, false, null, "&context=this is where the 'context' value goes");
+await api.AddRecord("did:example", "did:example:1234", "DIDCTXT", 600, false, null, "&context=this is where the 'context' value goes");
 
 // DIDAKA
-//await api.AddRecord("did:example", "DIDAKA", 600, false, null, "&alsoKnownAs=the 'AKA' value goes here...");
+await api.AddRecord("did:example", "did:example:1234", "DIDAKA", 600, false, null, "&alsoKnownAs=the 'AKA' value goes here...");
 
 // DIDCTLR
-//await api.AddRecord("did:example", "DIDCTLR", 600, false, null, "&controller=the 'controller' value goes here...");
+await api.AddRecord("did:example", "did:example:1234", "DIDCTLR", 600, false, null, "&controller=the 'controller' value goes here...");
 
+
+// This library containers DIDComm objects in which have method to convert to URI's for the API.
 
 // [VERIFICATION METHOD MAP]
-// Verification method map DID records follow a different pattern.
-// These are the record types for verification methods:
+// These are the record types you can use for verification method objects:
 
 // DIDVM
 // DIDAUTH
@@ -47,30 +45,30 @@ Console.WriteLine("[CREATE TXT RECORD]");
 // DIDCI
 // DIDCD
 
-// create the verification method map object
+// create the vmm object you want to send to DNS server
 DnsServerAPI.DIDComm.VerificationMethodMap vmm = new()
 {
     Id = "did:example:1234#key-1",
     Controller = "did:example:1234",
     Comment = "example comment.",
     Type_ = "Ed25519VerificationKey2018",
-    PublicKeyMultibase = "111222333",
-    PublicKeyJwk = new DnsServerAPI.DIDComm.JSONKeyMap()
-    {
-        crv = "crv",
-        e = "e",
-        n = "n",
-        x = "x",
-        y = "y",
-        kty = "kty",
-        kid = "kid",
-    },
-    PublicKeyBase58 = "123456",
+    //PublicKeyMultibase = "111222333",
+    //PublicKeyJwk = new DnsServerAPI.DIDComm.JSONKeyMap()
+    //{
+    //    crv = "crv",
+    //    e = "e",
+    //    n = "n",
+    //    x = "x",
+    //    y = "y",
+    //    kty = "kty",
+    //    kid = "kid",
+    //},
+    //PublicKeyBase58 = "123456",
     PrivateKeyBase58 = "abcdefg"
 };
 
-// pass the vmm object into this method, it will parse all the properties into a URI string for the api
-string s = await api.ADDRecordVMM("did:example", "did:example:111", "DIDKA", 600, false, "comment", vmm);
+// use the ToUri() method to get the url parameters for the verification method map
+string s = await api.AddRecord("did:example", "did:example:111", "DIDKA", 600, false, "comment", vmm.ToUri());
 
 Console.WriteLine(s);
 
