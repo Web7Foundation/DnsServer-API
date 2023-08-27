af4461b38b2fc7233a813b900dae80be2b413c45
Rustu Koprulu <rustulp@gmail.com>
2023-08-26 23:38:42 -0600

    Add Service Map and VMM ToUpdateUri() functions.

--- src/DnsServerAPI/DIDComm.cs
+++ src/DnsServerAPI/DIDComm.cs
@@ -11,6 +11,26 @@ public class VerificationMethodMap
     public string publicKeyMultibase { get; set; }             // STRING (Json Text)
     public string publicKeyJwk { get; set; }
 
+
+    /// <summary>
+    /// Generates a URI that can be used to send an update request.
+    /// </summary>
+    public string ToUpdateUri(VerificationMethodMap newVmm)
+    {
+        return this.ToUri() +
+            "&new_vmm_id=" + Uri.EscapeDataString(newVmm.Id) +
+            "&new_vmm_controller=" + Uri.EscapeDataString(newVmm.Controller) +
+            "&new_vmm_type=" + Uri.EscapeDataString(newVmm.Type_) +
+            "&new_vmm_comment=" + Uri.EscapeDataString(newVmm.Comment) +
+            "&new_vmm_keyPublicJsonWebKey=" + Uri.EscapeDataString(newVmm.keyPublicJsonWebKey) +
+            "&new_vmm_keyPublicJsonWebKeyString=" + Uri.EscapeDataString(newVmm.keyPublicJsonWebKeyString) +
+            "&new_vmm_publicKeyMultibase=" + Uri.EscapeDataString(newVmm.publicKeyMultibase) +
+            "&new_vmm_publicKeyJwk=" + Uri.EscapeDataString(newVmm.publicKeyJwk);
+    }
+
+    /// <summary>
+    /// Generates a URI that can be used to send an add or delete request.
+    /// </summary>
     public string ToUri()
     {
         return "&vmm_controller=" + Uri.EscapeDataString(Controller) +
@@ -33,6 +53,16 @@ public class ServiceMap
     public string Type_ { get; set; }
 
     public string ServiceEndpoint { get; set; }
+
+
+    public string ToUpdateUri(ServiceMap newSm)
+    {
+        return this.ToUri() +
+            "&new_sm_id=" + Uri.EscapeDataString(newSm.Id) +
+            "&new_sm_serviceEndpoint=" + Uri.EscapeDataString(newSm.ServiceEndpoint) +
+            "&new_sm_type=" + Uri.EscapeDataString(newSm.Type_) +
+            "&new_sm_comment=" + Uri.EscapeDataString(newSm.Comment);
+    }
 
     public string ToUri()
     {
